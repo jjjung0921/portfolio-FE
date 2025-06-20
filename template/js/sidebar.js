7c21842230c652d868708fc2aeee19963acce313
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-06-21 00:16:55 +0900

    Design: 사이드바 디자인 수정

--- template/js/sidebar.js
+++ template/js/sidebar.js
@@ -1,6 +1,8 @@
 const control_sidebar = () => {
     const sidebar = document.getElementById('left-sidebanner');
+    const menuBtn = document.getElementById('menu-btn');
     
     //sidebar 접고 펼치기
     sidebar.classList.toggle('activate'); // 'activate' 클래스 토글
+    menuBtn.classList.toggle('activate');
 }
